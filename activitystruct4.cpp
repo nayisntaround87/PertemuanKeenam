370b5aa8d86561d22153bc5e11bb16cf8c1ab282
nayisntaround87 <naylasalwahayati@gmail.com>
2024-04-02 15:01:27 +0700

    mengubah tanda kurung

--- activitystruct4.cpp
+++ activitystruct4.cpp
@@ -29,12 +29,14 @@ int main(){
          cout << "Umur Mahasiswa : ";
          cin >> mhs.umur;
     }
+    for (int i=0;i<3;i++){
+        cout << endl;
+        cout << "\n NIM : " << mhs.nim;
+        cout << "\n Nama : " << mhs.nama;
+        cout << "\n Alamat ";
+        cout << "\n \t Desa : " << mhs.alamat.desa;
+        cout << "\n \t Kota : " << mhs.alamat.kota;
+        cout << "\n Umur : " << mhs.umur;
+    }
 
-    cout << endl;
-    cout << "\n NIM : " << mhs.nim;
-    cout << "\n Nama : " << mhs.nama;
-    cout << "\n Alamat ";
-    cout << "\n \t Desa : " << mhs.alamat.desa;
-    cout << "\n \t Kota : " << mhs.alamat.kota;
-    cout << "\n Umur : " << mhs.umur;
 }
